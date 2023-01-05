54d65d67883a238bbe4936fe91590f1491e30b11
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2023-01-05 15:57:06 +0900

    [Feat]: set theme and dnd

---
+++ src/styled.ts
@@ -0,0 +1,8 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    boardColor: string;
+    cardColor: string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  boardColor: "#DADFE9",
+  cardColor: "white",
+};
